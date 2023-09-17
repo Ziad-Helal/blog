e2d1cfe70abf1029b061e86072b1031de27b852f
Ziad <zgalhelaly@gmail.com>
2023-09-17 03:41:53 +0300

    init

--- src/components/NavMenu/NavSubMenu/NavSubMenu.js
+++ src/components/NavMenu/NavSubMenu/NavSubMenu.js
@@ -2,22 +2,24 @@ import { Link } from "react-router-dom";
 import "./NavSubMenu.css";
 
 export default function NavSubMenu({ item, subMenu, className, clicked }) {
-    const clickHandler = () => {
-        clicked();
-    };
-    return (
-        <ul
-            className={`sub_menu${
-                window.matchMedia("(max-width: 559px)").matches && className ? className : ""
-            }`}
-        >
-            {subMenu.map((subItem) => (
-                <li key={subItem}>
-                    <Link to={`/${item}/${subItem}`} onClick={clickHandler}>
-                        {subItem}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    );
+  const clickHandler = () => {
+    clicked();
+  };
+  return (
+    <ul
+      className={`sub_menu${
+        window.matchMedia("(max-width: 559px)").matches && className
+          ? className
+          : ""
+      }`}
+    >
+      {subMenu.map((subItem) => (
+        <li key={subItem}>
+          <Link to={`/blog/${item}/${subItem}`} onClick={clickHandler}>
+            {subItem}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
--- src/layouts/MainLayout/MainLayoutFooter/MainLayoutFooterTop/MainLayoutFooterTop.js
+++ src/layouts/MainLayout/MainLayoutFooter/MainLayoutFooterTop/MainLayoutFooterTop.js
@@ -3,22 +3,22 @@ import { navItems } from "../../../../assets/data";
 import "./MainLayoutFooterTop.css";
 
 export default function MainLayoutFooterTop() {
-    return (
-        <section className="page__footer__top">
-            <Slider />
-            <nav>
-                <h2 className="screen-readers-only">Genre Navigation Menu</h2>
-                <ul className="genre__navigation">
-                    {navItems.map(
-                        ({ item, subMenu }) =>
-                            item === "Genre" &&
-                            subMenu.map((item) => (
-                                <NavItem key={item} item={item} footerNav={true} />
-                            ))
-                    )}
-                </ul>
-            </nav>
-            <ContactLinks />
-        </section>
-    );
+  return (
+    <section className="page__footer__top">
+      <Slider />
+      <nav>
+        <h2 className="screen-readers-only">Genre Navigation Menu</h2>
+        <ul className="genre__navigation">
+          {navItems.map(
+            ({ item, subMenu }) =>
+              item === "Genre" &&
+              subMenu.map((item) => (
+                <NavItem key={item} item={item} footerNav={true} />
+              ))
+          )}
+        </ul>
+      </nav>
+      <ContactLinks />
+    </section>
+  );
 }
